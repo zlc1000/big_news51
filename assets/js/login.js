a3e55afff6d2306854afba5c3440858e543f0384
zhanglianchang <zhanglianchang@itcast.cn>
2022-09-26 17:23:09 +0800

    简单回执了一下登录和注册区域

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+$(function () {
+  // 点击去注册
+  $('#go2Reg').on('click', function () {
+    $('.login-wrap').hide()
+    $('.reg-wrap').show()
+  })
+
+  // 点击去登录
+  $('#go2Login').on('click', function () {
+    $('.reg-wrap').hide()
+    $('.login-wrap').show()
+  })
+
+  // 自己预习的时候，最好把文档通读一遍
+})
