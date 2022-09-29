47b1f92e3a29cfdaa3d7a9b8ceae096655f0385b
zhanglianchang <zhanglianchang@itcast.cn>
2022-09-29 17:14:10 +0800

    添加了index首页静态页面布局

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -10,7 +10,7 @@ $.ajaxPrefilter(function (config) {
     })
     return JSON.stringify(target)
   }
-  
+
   // 统一设置基准地址
   config.url = 'http://big-event-vue-api-t.itheima.net' + config.url
 
@@ -20,3 +20,11 @@ $.ajaxPrefilter(function (config) {
   // 统一设置请求的参数 - post 请求
   config.data = format2Json(config.data)
 })
+
+/**
+ * 仓库：https://github.com/zlc1000/big_news51.git
+ *
+ * home分支: git add .  -> git commit -m '完成了登录和注册功能' -> git push
+ * git checkout mater -> git merge home -> git push
+ * git checkout -b index -> 再继续开发首页的功能就好了
+ */
